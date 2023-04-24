a459dc5bf760c9ff9c7e218c7ce54ea8dfb6d16b
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-04-24 09:15:37 -0500

    feat: :sparkles: Buscador Peliculas
    se agrego el modal con la api

--- index.js
+++ index.js
@@ -35,13 +35,57 @@ function printHTML(cards){
             <p>Pelicula</p>
     </div>
     <ul class="list-group list-group-flush bg-dark">
-        <li class="list-group-item bg-dark"style="color:white;">Type: ${Type}</li>
-        <li class="list-group-item bg-dark"style="color:white;">Year: ${Year}</li>
-        <button class="bg-success" style="border-color:transparent; color:white">Mostar mas</button>
+        <li class="list-group-item bg-dark"style="border-color:transparent; color:white;">Type: ${Type}</li>
+        <li class="list-group-item bg-dark"style="border-color:transparent; color:white;">Year: ${Year}</li>
+        <button class="btn btn-primary btnDetalle" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" img="${Poster}" titulo="${Title}" year="${Year}" id="${imdbID}" tipo="${Type}">Mas Info</button>
+      
      </ul>
     </div>
         `
     })  
     contenido.innerHTML = plantilla
-    
+
+    contenido.addEventListener('click', (e) => {
+        let htmlTitle = '';
+        let htmlModal = '';
+        let htmlTable = '';
+        const modalTitle = document.querySelector('.modal-title');
+        const modalBody = document.querySelector('.modal-body');
+  
+        const btnDetalle = e.target.classList[2];
+        if (btnDetalle == 'btnDetalle') {
+          const img = e.target.getAttribute("img");
+          const titulo = e.target.getAttribute("titulo");
+          const year = e.target.getAttribute("year");
+          const id = e.target.getAttribute("id");
+          const type = e.target.getAttribute("tipo");
+          htmlModal += `
+          <img src="${img}">
+          <table class="table text-light">
+                  <thead>
+                    <tr>
+                      <th scope="col">Pelicula</th>
+                      <th scope="col">Año</th>
+                      <th scope="col">Id</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>${titulo}</td>
+                      <td>${year}</td>
+                      <td>${id}</td>
+                    </tr>
+                   
+                  </tbody>
+                </table>
+          `
+          htmlTitle += `${titulo}`
+          modalBody.innerHTML = htmlModal + htmlTable;
+          modalTitle.innerHTML = htmlTitle;
+  
+        } else {
+          console.log('Ño');
+        }
+       
+      })
 }
